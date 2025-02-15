2d19c141c2af9960570538af39d66091df40ab63
ByCheno <jcoronelcamacho@gmail.com>
2025-02-15 20:42:52 +0100

    Admin panel

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,8 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField() 
-    image = models.ImageField(upload_to='portfolio/images/')
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True) 
+    title = models.CharField(max_length=100, verbose_name = 'titulo')
+    description = models.TextField(verbose_name = 'descripcion')
+    image = models.ImageField(upload_to='img', verbose_name = 'imagen')
+    created = models.DateTimeField(auto_now_add=True, verbose_name = 'Fecha de creacion')
+    updated = models.DateTimeField(auto_now=True, verbose_name = 'Fecha actualizacion')
+    
+    class Meta:
+        verbose_name = 'proyecto'
+        verbose_name_plural = 'proyectos'
+        ordering = ['-created']
+        
+    def __str__(self):
+        return self.title
