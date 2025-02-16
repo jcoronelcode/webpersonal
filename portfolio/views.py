facec4b43834b26497dcf2c4e5525fef99db2a77
ByCheno <jcoronelcamacho@gmail.com>
2025-02-16 14:00:09 +0100

    View portfolio done

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,4 +4,4 @@ from .models import Project
 
 def portfolio(request):
     projects = Project.objects.all()
-    return render(request, 'portfolio/portfolio.html')
+    return render(request, 'portfolio/portfolio.html', {'projects':projects})
